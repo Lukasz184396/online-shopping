eb6c4b21912974c434e04ce84feb3c46dc2ec699
Lukasz184396 <zawadalukasz88@gmail.com>
2018-02-27 22:33:35 +0100

    Add Product java part and test cases

--- shoppingbackend/databaseQueries.sql
+++ shoppingbackend/databaseQueries.sql
@@ -38,6 +38,8 @@ INSERT INTO user_detail
 (first_name, last_name, role, enabled, password, email, contact_number) 
 VALUES ('Ravichandra', 'Ashwin', 'SUPPLIER', true, '12345', 'ra@gmail.com', '7777777777');
 
+
+
 CREATE TABLE product (
 	id IDENTITY,
 	code VARCHAR(20),
@@ -46,26 +48,28 @@ CREATE TABLE product (
 	description VARCHAR(255),
 	unit_price DECIMAL(10,2),
 	quantity INT,
-	category_id INT,
-	supplier_id INT,
+	is_active BOOLEAN,
+ 	category_id INT,
+ 	supplier_id INT,
+	purchases INT DEFAULT 0,
+	views INT DEFAULT 0,
 	CONSTRAINT pk_product_id PRIMARY KEY (id),
  	CONSTRAINT fk_product_category_id FOREIGN KEY (category_id) REFERENCES category (id),
 	CONSTRAINT fk_product_supplier_id FOREIGN KEY (supplier_id) REFERENCES user_detail(id),	
 );	
 
 
-INSERT INTO product (code, name, brand, description, unit_price, quantity, category_id, supplier_id)
-VALUES ('PRDABC123DEFX', 'iPhone 5s', 'apple', 'This is one of the best phone available in the market right now!', 18000, 5, 3, 2 );
+INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id)
+VALUES ('PRDABC123DEFX', 'iPhone 5s', 'apple', 'This is one of the best phone available in the market right now!', 18000, 5, true, 3, 2 );
+ 
+INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id)
+VALUES ('PRDDEF123DEFX', 'Samsung s7', 'samsung', 'A smart phone by samsung!', 32000, 2, true, 3, 3 );
 
-INSERT INTO product (code, name, brand, description, unit_price, quantity, category_id, supplier_id)
-VALUES ('PRDDEF123DEFX', 'Samsung s7', 'samsung', 'A smart phone by samsung!', 32000, 2, 3, 3 );
-
-INSERT INTO product (code, name, brand, description, unit_price, quantity, category_id, supplier_id)
-VALUES ('PRDMNO123PQRX', ' Macbook Pro', 'apple', 'This is one of the best laptops available in the market right now!', 54000, 3, 1, 2 );
-
-INSERT INTO product (code, name, brand, description, unit_price, quantity, category_id, supplier_id)
-VALUES ('PRDPQR123WGTX', 'Google Pixel', 'google', 'This is one of the best android smart phone available in the market right now!', 57000, 5, 3, 2 );
-
-INSERT INTO product (code, name, brand, description, unit_price, quantity, category_id, supplier_id)
-VALUES ('PRDABCXYZDEFX', 'Dell Latitude E6510', 'dell', 'This is one of the best laptop series from dell that can be used!', 48000, 5, 3, 3 );
-  
+INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id)
+VALUES ('PRDPQR123WGTX', 'Google Pixel', 'google', 'This is one of the best android smart phone available in the market right now!', 57000, 5, true, 3, 2 );
+ 
+INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id)
+VALUES ('PRDMNO123PQRX', ' Macbook Pro', 'apple', 'This is one of the best laptops available in the market right now!', 54000, 3, true, 1, 2 );
+ 
+INSERT INTO product (code, name, brand, description, unit_price, quantity, is_active, category_id, supplier_id)
+VALUES ('PRDABCXYZDEFX', 'Dell Latitude E6510', 'dell', 'This is one of the best laptop series from dell that can be used!', 48000, 5, true, 1, 3 );

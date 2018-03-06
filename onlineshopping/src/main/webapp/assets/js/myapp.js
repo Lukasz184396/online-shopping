65c792ac26e01433fd2c376ebdce4271f8bfbf60
Lukasz184396 <zawadalukasz88@gmail.com>
2018-03-06 22:22:50 +0100

    Added Product Management form draft

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,108 +1,126 @@
 $(function() {
-	//solving  active menu problem
-	switch(menu)	{
-		
-		case 'About Us' :
-			$('#about').addClass('active');
-			break;
-		case 'Contact Us' :
-			$('#contact').addClass('active');
-			break;
-		case 'All Products' :
-			$('#listProducts').addClass('active');
-			break;
-		default:
-			if(menu == "Home") break;			
-			$('#listProducts').addClass('active');
-			$('#a_'+menu).addClass('active');	
-			break;
-	}
-	
-	//code for jquery dataTable	
-	
+	// solving active menu problem
+	switch (menu) {
 
-	//get that table	
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	case 'All Products':
+		$('#listProducts').addClass('active');
+		break;
+	case 'Product Management':
+		$('#manageProduct').addClass('active');
+		break;
+	default:
+		if (menu == "Home")
+			break;
+		$('#listProducts').addClass('active');
+		$('#a_' + menu).addClass('active');
+		break;
+	}
+
+	// code for jquery dataTable
+
+	// get that table
 	var $table = $('#productListTable');
-	
-	//execute the below code only where  we have this table
-	if($table.length) {
-		
-		//console.log('Inside the table!')
-		
-		var jsonUrl = '';
-		if(window.categoryId == '') {
-			jsonUrl = window.contextRoot + '/json/data/all/products';
-		}
-		else {
-			jsonUrl = window.contextRoot + '/json/data/category/'+ window.categoryId + '/products';
-		}
-		
-		$table.DataTable( {
-			lengthMenu: [ [3, 5, 10, -1], ['3 Records', '5 Records','10 Records','ALL Records'] ],
-			pageLength: 5,
-			ajax: {
-				url: jsonUrl,
-				dataSrc: ''
-			},
-			columns: [
-			          
-			          {
-			        	  data: 'code',
-			        	  bSortable: false,
-			        	  mRender: function(data, type, row) {
-			        	  		        		  
-			        	  return '<img src="'+window.contextRoot+'/resources/images/'+data+'.jpg" class="dataTableImg"/>';
-			        	  			        		  
-			        	  			        	  }
-			          },
-			          {
-			        	  data: 'name'
-			          },
-			          {
-			        	  data: 'brand'
-			          },
-			          {
-			        	  data: 'unitPrice',
-			        	  mRender: function(data, tpye, row) {
-			        		  return '\u20AC ' + data
-			        	  }
-			          },
-			          {
-			        	  data: 'quantity',
-			        	  mRender: function(data, type, row) {
-			        		  if(data < 1) {
-			        			  return '<span style="color:red">Out of Stock!</span>'; 
-			        		  }
-			        		  return data;
-			        	  }
-			       
-			          },
-			          {
-			        	  data: 'id',
-			        	  bSortable: false,
-			        	  mRender: function(data, tpye, row) {
-			        		  var str = '';
-			        		  str += '<a href="'+window.contextRoot+ '/show/'+data+'/product " class="btn btn-priamry"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';//&#160; is for addition space between icons
-			        		  
-			        		  
-			        		  if(row.quantity < 1) {
-			        		    str += '<a href="javascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
-			        		  }
-			        		  else {			        			  
-			        			str += '<a href="'+window.contextRoot+ '/cart/add/'+data+'/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';  
-			        		  }
-			        		  
-			        		   return str;
-			        	  }
-			          }
-			          	
-			          
-			          ]
-			
-			
-		});
-		
-	}
-	
-});
 
+	// execute the below code only where we have this table
+	if ($table.length) {
+
+		// console.log('Inside the table!')
+
+		var jsonUrl = '';
+		if (window.categoryId == '') {
+			jsonUrl = window.contextRoot + '/json/data/all/products';
+		} else {
+			jsonUrl = window.contextRoot + '/json/data/category/'
+					+ window.categoryId + '/products';
+		}
+
+		$table
+				.DataTable({
+					lengthMenu : [
+							[ 3, 5, 10, -1 ],
+							[ '3 Records', '5 Records', '10 Records',
+									'ALL Records' ] ],
+					pageLength : 5,
+					ajax : {
+						url : jsonUrl,
+						dataSrc : ''
+					},
+					columns : [
+
+							{
+								data : 'code',
+								bSortable : false,
+								mRender : function(data, type, row) {
+
+									return '<img src="' + window.contextRoot
+											+ '/resources/images/' + data
+											+ '.jpg" class="dataTableImg"/>';
+
+								}
+							},
+							{
+								data : 'name'
+							},
+							{
+								data : 'brand'
+							},
+							{
+								data : 'unitPrice',
+								mRender : function(data, tpye, row) {
+									return '\u20AC ' + data
+								}
+							},
+							{
+								data : 'quantity',
+								mRender : function(data, type, row) {
+									if (data < 1) {
+										return '<span style="color:red">Out of Stock!</span>';
+									}
+									return data;
+								}
+
+							},
+							{
+								data : 'id',
+								bSortable : false,
+								mRender : function(data, tpye, row) {
+									var str = '';
+									str += '<a href="'
+											+ window.contextRoot
+											+ '/show/'
+											+ data
+											+ '/product " class="btn btn-priamry"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';// &#160;
+																																					// is
+																																					// for
+																																					// addition
+																																					// space
+																																					// between
+																																					// icons
+
+									if (row.quantity < 1) {
+										str += '<a href="javascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+									} else {
+										str += '<a href="'
+												+ window.contextRoot
+												+ '/cart/add/'
+												+ data
+												+ '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+									}
+
+									return str;
+								}
+							}
+
+					]
+
+				});
+
+	}
+
+});
